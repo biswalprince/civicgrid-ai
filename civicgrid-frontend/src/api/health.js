3e111c954be3b1c110f7c009b155abab9658499d
Prince Biswal <biswalprince1@gmail.com>
2026-09-26 20:30:39 +0530

    Clean up frontend API integration

--- civicgrid-frontend/src/api/health.js
+++ civicgrid-frontend/src/api/health.js
@@ -1,6 +1,6 @@
 import api from './axios'
 
 export async function checkHealth() {
-  const response = await api.get('/health/')
+  const response = await api.get('/api/health/')
   return response.data
 }
--- civicgrid-frontend/src/api/hotspots.js
+++
@@ -1,6 +0,0 @@
-import api from './axios'
-
-export async function getHotspots() {
-  const response = await api.get('api/hotspots/')
-  return response.data
-}
